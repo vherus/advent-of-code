7a89d676153a94c36dfb69d633f89b5ec69165f1
Nathan King <nathanking3590@gmail.com>
2023-12-01 11:48:16 +0000

    update solution

--- 2023/1/index.js
+++ 2023/1/index.js
@@ -1,50 +1,46 @@
 const { open } = require('fs').promises
 
-async function partOne(fileName) {
+async function run(fileName, lineFunc) {
     const input = await open(fileName)
     let sum = 0
 
     for await (const l of input.readLines()) {
-        const nums = l.match(/\d/g)
-        sum += Number(nums[0] + nums[nums.length-1])
+        sum += lineFunc(l)
     }
 
     return sum
 }
 
-partOne(__dirname + '/input.txt').then(console.log)
+function partOne(line) {
+    const nums = line.match(/\d/g)
+    return Number(nums[0] + nums[nums.length-1])
+}
 
-async function partTwo(fileName) {
+function partTwo(line) {
     const numMap = { one: '1', two: '2', three: '3', four: '4', five: '5', six: '6', seven: '7', eight: '8', nine: '9' }
-    const input = await open(fileName)
-    let sum = 0
+    const nums = line
+        .replace('oneight', 'oneeight')
+        .replace('twone', 'twoone')
+        .replace('threeight', 'threeeight')
+        .replace('fiveight', 'fiveeight')
+        .replace('sevenine', 'sevennine')
+        .replace('nineight', 'nineeight')
+        .replace('eighthree', 'eightthree')
+        .replace('eightwo', 'eighttwo')
+        .match(/(?:one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9)/g)
 
-    for await (const l of input.readLines()) {
-        const nums = l
-            .replace('oneight', 'oneeight')
-            .replace('twone', 'twoone')
-            .replace('threeight', 'threeeight')
-            .replace('fiveight', 'fiveeight')
-            .replace('sevenine', 'sevennine')
-            .replace('nineight', 'nineeight')
-            .replace('eighthree', 'eightthree')
-            .replace('eightwo', 'eighttwo')
-            .match(/(?:one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9)/g)
-        
-        let num = nums[0].length > 1 ? numMap[nums[0]] : nums[0]
+    let num = nums[0].length > 1 ? numMap[nums[0]] : nums[0]
 
-        if (nums.length === 1) {
-            num += num
-        }
-
-        if (nums.length > 1) {
-            num += nums[nums.length-1].length > 1 ? numMap[nums[nums.length-1]] : nums[nums.length-1]
-        }
-
-        sum += Number(num)
+    if (nums.length === 1) {
+        num += num
     }
 
-    return sum
+    if (nums.length > 1) {
+        num += nums[nums.length-1].length > 1 ? numMap[nums[nums.length-1]] : nums[nums.length-1]
+    }
+
+    return Number(num)
 }
 
-partTwo(__dirname + '/input.txt').then(console.log)
+run(__dirname + '/input.txt', partOne).then(console.log)
+run(__dirname + '/input.txt', partTwo).then(console.log)
